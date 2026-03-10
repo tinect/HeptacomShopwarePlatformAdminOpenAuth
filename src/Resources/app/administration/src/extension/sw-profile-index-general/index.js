99c4a2869df220860ab7d759a82cce07a7065fed
tinect <s.koenig@tinect.de>
2026-03-10 11:38:39 +0100

    feat: show SSO status in user details
    Closes #40

--- src/Resources/app/administration/src/extension/sw-profile-index-general/index.js
+++ src/Resources/app/administration/src/extension/sw-profile-index-general/index.js
@@ -1,44 +1,20 @@
 import template from './sw-profile-index-general.html.twig';
 
-const { Component, Context } = Shopware;
+const { Component } = Shopware;
 
 Component.override('sw-profile-index-general', {
     template,
 
     inject: [
-        'repositoryFactory',
         'systemConfigApiService',
+        'loginService'
     ],
 
     data() {
         return {
             denyPasswordLogin: false,
-            heptacomAdminOpenAuthLoading: true,
-            heptacomAdminOpenAuthClients: [],
+            userId: null,
         }
-    },
-
-    computed: {
-        heptacomAdminOpenAuthClientsRepository() {
-            return this.repositoryFactory.create('heptacom_admin_open_auth_client')
-        },
-
-        heptacomAdminOpenAuthHttpClient() {
-            return this.heptacomAdminOpenAuthClientsRepository.httpClient;
-        },
-    },
-
-    watch: {
-        isUserLoading: {
-            handler() {
-                this.loadHeptacomAdminOpenAuth().then()
-            },
-        },
-        languages: {
-            handler() {
-                this.loadHeptacomAdminOpenAuth().then()
-            },
-        },
     },
 
     created() {
@@ -46,38 +22,6 @@ Component.override('sw-profile-index-general', {
             this.denyPasswordLogin = response['KskHeptacomAdminOpenAuth.config.denyPasswordLogin'];
         });
 
-        this.loadHeptacomAdminOpenAuth();
-    },
-
-    methods: {
-        async loadHeptacomAdminOpenAuth() {
-            this.heptacomAdminOpenAuthLoading = true;
-            this.heptacomAdminOpenAuthClients = [];
-
-            const headers = this.heptacomAdminOpenAuthClientsRepository.buildHeaders(Context.api);
-            const response = await this.heptacomAdminOpenAuthHttpClient
-                .get(`/_admin/open-auth/client/list`, { headers });
-
-            this.heptacomAdminOpenAuthClients = response.data.data;
-            this.heptacomAdminOpenAuthLoading = false;
-        },
-
-        async redirectToLoginMask(clientId) {
-            const currentPath = window.location.pathname + window.location.search + window.location.hash;
-
-            const headers = this.heptacomAdminOpenAuthClientsRepository.buildHeaders(Context.api);
-            const response = await this.heptacomAdminOpenAuthHttpClient
-                .post(`/_action/open-auth/${clientId}/connect?redirectTo=${encodeURIComponent(currentPath)}`, {}, { headers });
-
-            window.location.href = response.data.target;
-        },
-
-        async revokeHeptacomAdminOpenAuthUserKey(clientId) {
-            const headers = this.heptacomAdminOpenAuthClientsRepository.buildHeaders(Context.api);
-            await this.heptacomAdminOpenAuthHttpClient
-                .post(`/_action/open-auth/${clientId}/disconnect`, {}, { headers });
-
-            await this.loadHeptacomAdminOpenAuth();
-        },
+        this.userId = Shopware.Store.get('session')?.currentUser.id;
     },
 });
